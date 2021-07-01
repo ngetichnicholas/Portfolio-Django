360e80983b7b20119b895db3e4c1582a207e8c38
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-01 21:52:38 +0300

    Add model class for project author

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Author(models.Model):
+  first_name = models.CharField(max_length=30)
+  sur_name = models.CharField(max_length=30)
+  email = models.EmailField()
+  phone_number = models.CharField(max_length=10,blank=True)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Project
 
 # Create your views here.
