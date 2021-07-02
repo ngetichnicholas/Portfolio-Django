09ba52b655ac8b2850ec26dfd88a7bb17f9dbeac
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-02 13:30:12 +0300

    Add search template

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   url(r'^$', views.home, name='homePage'),
-  url(r'projects/', views.projects, name='all_projects'),
-  url(r'^search/',views.)
+  url(r'projects/', views.projects, name='allProjects'),
+  url(r'^search/',views.search_projects,name='searchProjects'),
+  url(r'^project/(d+)',views.project, name='project')
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
